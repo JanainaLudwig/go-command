0c33518eb974bf8c86769f9b5a4244117f9ec155
JanainaLudwig <janainaludwig@gmail.com>
2021-05-02 22:03:18 -0300

    project added

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-package golang_command
+package main
+
+func main()  {
+	alexa := &Alexa{}
+	swit := &Switch{}
+
+	onAlexa := OnCommand{lamp: alexa}
+	onSwitch := OnCommand{lamp: swit}
+
+	onAlexa.execute()
+	onSwitch.execute()
+
+
+	changeColorAlexa := ChangeColorCommand{lamp: alexa}
+	changeColorSwitch := ChangeColorCommand{lamp: swit}
+
+	changeColorAlexa.execute()
+	changeColorSwitch.execute()
+
+
+	offAlexa := ChangeColorCommand{lamp: alexa}
+	offSwitch := ChangeColorCommand{lamp: swit}
+
+	offAlexa.execute()
+	offSwitch.execute()
+}
