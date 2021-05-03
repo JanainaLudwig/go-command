8a99d0dc44b8e61a77769227ad950ed2ada51189
JanainaLudwig <janainaludwig@gmail.com>
2021-05-02 21:51:33 -0300

    Initial project

---
+++ command.go
@@ -0,0 +1,33 @@
+package golang_command
+
+type Command interface {
+	execute()
+}
+
+// OnCommand - turns the lamp on
+type OnCommand struct {
+	lamp Lamp
+}
+
+func (o *OnCommand) execute() {
+	o.lamp.on()
+}
+
+
+// OffCommand - turns the lamp off
+type OffCommand struct {
+	lamp Lamp
+}
+
+func (o *OffCommand) execute() {
+	o.lamp.off()
+}
+
+// ChangeColorCommand - turns the lamp off
+type ChangeColorCommand struct {
+	lamp Lamp
+}
+
+func (o *ChangeColorCommand) execute() {
+	o.lamp.changeColor()
+}
---
+++ receiver.go
@@ -0,0 +1,50 @@
+package golang_command
+
+import "log"
+
+type Lamp interface {
+	on()
+	off()
+	changeColor()
+}
+
+type Switch struct {
+	lampOn bool
+	color string
+}
+
+func (s *Switch) on()  {
+	log.Println("[Switch] turning on...")
+	s.lampOn = true
+}
+
+func (s *Switch) off()  {
+	log.Println("[Switch] turning off...")
+	s.lampOn = false
+}
+
+func (s *Switch) changeColor()  {
+	log.Println("[Switch] changing color...")
+	s.color = "green"
+}
+
+
+type Alexa struct {
+	lampOn bool
+	lampColor string
+}
+
+func (s *Alexa) on()  {
+	log.Println("[Alexa] turning on...")
+	s.lampOn = true
+}
+
+func (s *Alexa) off()  {
+	log.Println("[Alexa] turning off...")
+	s.lampOn = false
+}
+
+func (s *Alexa) changeColor()  {
+	log.Println("[Alexa] changing color...")
+	s.lampColor = "red"
+}
